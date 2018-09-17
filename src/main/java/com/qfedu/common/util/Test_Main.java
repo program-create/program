6fd64c0d467d27445048de71e57fd07972b2ffe1
Ideal-Xiang <13723275355@163.com>
2018-09-17 22:02:59 +0800

    李想提交

--- src/main/java/com/qfedu/common/util/Test_Main.java
+++ src/main/java/com/qfedu/common/util/Test_Main.java
@@ -1,6 +1,11 @@
 package com.qfedu.common.util;
 
 public class Test_Main {
+    public static void aaa() {
+        System.out.println("李想提交");
+    }
+
+
     public static void main (String[] args) {
         System.out.println("12345:" + ShiroEncryUtil.md5("12345"));
     }
